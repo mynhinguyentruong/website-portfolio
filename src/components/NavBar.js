4bbb07cc9fa82b018ff1cccd2e727e64a73b1f54
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-07-29 11:55:53 -0400

    Revert "Create the ugliest transition ever"
    This reverts commit 4d2b14246f706d1c1e5281008954e0c7e814f845.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,18 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 
 import './NavBarStyles.css'
 
-import { FaBars, FaTimes } from "react-icons/fa"
+import { FaBars } from "react-icons/fa"
 
 function Navbar() {
-
-  const [toggleNavBar, setToggleNavBar] = useState(false)
-  const handleClick = () => setToggleNavBar(prevState => !prevState)
-
   return (
     <div className='header'>
-      <div className='hamburger' onClick={handleClick}>
-        {toggleNavBar ? <FaBars size={20} style={{color: '#fff'}}/> : <FaTimes size={20} style={{color: '#fff'}}/>}
-        
+      <div className='hamburger'>
+        <FaBars size={20} style={{color: '#fff'}}/>
       </div>
       <Link to='/'><h1 >Portfolio Logo</h1></Link>
-      <ul className={toggleNavBar ? "nav-menu-active" : "nav-menu"}>
+      <ul className='nav-menu'>
         <li><Link to='/'>Home</Link></li>
        
         <li><Link to='/project'>Project</Link></li>
