dc5091ae2b809c9afdc6335943a4c80775732003
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-07-28 21:49:03 -0400

    Set up basic Route, Link to components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import Navbar from "./components/NavBar";
 
+import Contact from "./routes/Contact";
+import Project from "./routes/Project";
+import Home from "./routes/Home";
+
+import { Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      Hi world
+      <Navbar/>
+      <Routes>
+        <Route exact path='/' element={<Home/>} />
+        <Route exact path='/project' element={<Project/>} />
+        <Route exact path='/contact' element={<Contact/>} />
+      </Routes>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,37 +1,14 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 
 function Navbar() {
   return (
-    <Navbar  expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">
-          <img src={''} alt="logo" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" >
-          <span className='navbar-toggler-icon'></span>
-        </Navbar.Toggle>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#skills">Skills</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
-          </Nav>
-          <span className='navbar-text'>
-            <div className='social-icon'>
-              Social icon here:
-              1
-              2
-              3
-            </div>
-            <button className='vvd' onClick={()=>console.log('connect')}>Let's Connect</button>
-          </span>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Link to='/'>Home</Link>
+      <Link to='/project'>Project</Link>
+      <Link to='/contact'>Contact</Link>
+    </>
   );
 }
 
