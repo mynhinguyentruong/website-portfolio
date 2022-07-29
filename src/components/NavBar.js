389a8c09f81492b036e854f33fe39395e7085ad4
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-07-28 20:42:09 -0400

    Delete unnecessary files in create-react-app installation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,8 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
+
+
 function Navbar() {
   return (
     <Navbar  expand="lg">
