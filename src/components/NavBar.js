151a10b0d31ca127ec27b9bb99602f4fa3746224
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-07-29 12:20:10 -0400

    Reset the styling of Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,18 +3,29 @@ import { Link } from 'react-router-dom'
 
 import './NavBarStyles.css'
 
+import { FaBars, FaTimes } from 'react-icons/fa'
 
 function Navbar() {
+
+const [click, setClick] = React.useState(false)
+
+const handleClick = () => {
+  setClick(prevClick => !prevClick)
+}
+
   return (
     <div className='header'>
-      <Link to='/'><h1 >Portfolio Logo</h1></Link>
-      <ul className='nav-menu'>
+      <Link to='/'><h1 >Portfolio</h1></Link>
+      <ul className={click ? "nav-menu active" : "nav-menu" }>
         <li><Link to='/'>Home</Link></li>
-       
         <li><Link to='/project'>Project</Link></li>
         <li><Link to='/contact'>Contact</Link></li>
       </ul>
-
+      <div className='hamburger' onClick={handleClick}>
+        {click ? 
+        <FaTimes size={20} style={{color: 'white', cursor:'pointer'}} /> : 
+        <FaBars size={20} style={{color: 'white', cursor:'pointer'}} />}
+      </div>
     </div >
   );
 }
