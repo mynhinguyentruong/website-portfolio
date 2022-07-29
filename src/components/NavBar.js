2f3ea9e228e75b4f51b4dc549a247b448712d345
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-07-29 11:56:37 -0400

    Revert "Added toggle icon at mobile screen"
    This reverts commit e017452521a9b9a3df18e4f69d0cc86ce7f5f83b.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,14 +3,10 @@ import { Link } from 'react-router-dom'
 
 import './NavBarStyles.css'
 
-import { FaBars } from "react-icons/fa"
 
 function Navbar() {
   return (
     <div className='header'>
-      <div className='hamburger'>
-        <FaBars size={20} style={{color: '#fff'}}/>
-      </div>
       <Link to='/'><h1 >Portfolio Logo</h1></Link>
       <ul className='nav-menu'>
         <li><Link to='/'>Home</Link></li>
@@ -18,7 +14,7 @@ function Navbar() {
         <li><Link to='/project'>Project</Link></li>
         <li><Link to='/contact'>Contact</Link></li>
       </ul>
-      
+
     </div >
   );
 }
