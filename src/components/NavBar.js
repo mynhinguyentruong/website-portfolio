e017452521a9b9a3df18e4f69d0cc86ce7f5f83b
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-07-28 22:33:43 -0400

    Added toggle icon at mobile screen

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,10 +3,14 @@ import { Link } from 'react-router-dom'
 
 import './NavBarStyles.css'
 
+import { FaBars } from "react-icons/fa"
 
 function Navbar() {
   return (
     <div className='header'>
+      <div className='hamburger'>
+        <FaBars size={20} style={{color: '#fff'}}/>
+      </div>
       <Link to='/'><h1 >Portfolio Logo</h1></Link>
       <ul className='nav-menu'>
         <li><Link to='/'>Home</Link></li>
@@ -14,7 +18,7 @@ function Navbar() {
         <li><Link to='/project'>Project</Link></li>
         <li><Link to='/contact'>Contact</Link></li>
       </ul>
-
+      
     </div >
   );
 }
