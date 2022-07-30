5bc5a4052e26e338004ad95b0a5fb887a4652670
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-07-29 20:49:51 -0400

    Setup Contact.js component

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,8 +1,27 @@
 import React from 'react'
 
+import "./ContactStyles.css"
+import IntroImg from '../images/intro-bg.jpg'
+
 export default function Contact() {
 
   return (
-    <h1>Contact</h1>
+    <div className='form'>
+      <div className='mask'>
+        <img className='into-img' src={IntroImg} alt="Intro Background" />
+      </div>
+      <h1>Let's connect</h1>
+      <form>
+        <label htmlFor='name'>Your Name</label>
+        <input id='name' type="text"/>
+        <label htmlFor='email'>Email</label>
+        <input id='email' type="email"/>
+        <label htmlFor='subject'>Subject</label>
+        <input id='subject' type="text"/>
+        <label htmlFor='message'>Message</label>
+        <textarea placeholder='Type your message here' id='message'/>
+        <button>Submit</button>
+      </form>
+    </div>
   )
 }
