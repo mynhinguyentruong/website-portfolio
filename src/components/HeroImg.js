ddc9282c242ca803296e1dbcff3c26d901f09c0f
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-07-29 20:21:42 -0400

    Finish Home Page component

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+
+import IntroImg from '../images/intro-bg.jpg'
+
+import './HeroImgStyles.css'
 
 export default function HeroImg() {
   return (
     <div className='hero'>
-      <h1>sadasdasd</h1>
+      <div className='mask'>
+        <img className='into-img' src={IntroImg} alt="Intro Background" />
+      </div>
+      <div className='content'>
+        <p>Hi, I'm Nhi</p>
+        <span>Your Friendly Neighbor...</span>
+        <h1>React Developer</h1>
+        <div>
+          <Link to path='/project' className='btn'>Projects</Link>
+          <Link to path='/contact' className='btn btn-light'>Contact</Link>
+        </div>
+      </div>
     </div>
   )
 }
